c1995c1267513594e2489f2128b25983526dacca
Den Hafiz <den.m.hafiz@gmail.com>
2023-01-15 09:39:49 +0800

    Added the loop for finding the details

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from bs4 import BeautifulSoup
-
-with open("index.html") as fp:
-    soup = BeautifulSoup(fp)
+import requests
 
 url = "https://www.mudah.my/kuala-lumpur/apartment-condominium-for-rent?q=studio"
+page = requests.get(url)
 
+soup = BeautifulSoup(page.content, 'html.parser')
+
+the_lists = soup.find_all('div', class_="sc-ksYbfQ eHFZkE")
+
+for i in the_lists:
+    title = i.find('a', class_="sc-uJMKN dQixCO")
+    location = i.find('span', class_="sc-caSCKo gabJBv")
+    price = i.find('div', class_="sc-jnlKLf bhfbqU")
+    posted_date = i.find('span', class_="sc-eqIVtm jQMyrz")
