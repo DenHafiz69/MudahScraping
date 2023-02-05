5093dec9e287e5d9c04ba441710368c237386c48
Den Hafiz <den.m.hafiz@gmail.com>
2023-02-05 23:26:25 +0800

    Check the data has been converted or not

--- data_cleaning.py
+++ data_cleaning.py
@@ -18,9 +18,10 @@ def extract_bed(text):
 
 # convert all the data from strings with unnecessary text into integers
 
-df['price'] = df['price'].apply(extract_price)
-df['price'] = df['size'].apply(extract_size)
-df['price'] = df['bedroom'].apply(extract_bed)
+if df['price'].dtype == str: # check whether data has been converted or not
+  df['price'] = df['price'].apply(extract_price)
+  df['price'] = df['size'].apply(extract_size)
+  df['price'] = df['bedroom'].apply(extract_bed)
 
 # save the modified DataFrame back to the CSV file
 
